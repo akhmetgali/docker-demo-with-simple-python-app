816ffb8b8ad672f31ea3c956f5282a3c484599cb
akhmetgali <akhmetgali@outlook.com>
2019-10-31 20:03:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Harness'
+    return 'Hello Qventu Team'
 
 @app.errorhandler(500)
 def server_error(e):
